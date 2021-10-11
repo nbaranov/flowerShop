ee419aab4252ecbcd81f69f9bd8ff934e18b5c0c
Nikita Baranov <nbaranov.vrn@gmail.com>
2021-10-11 18:22:12 +0300

    add pagination to ProductList APIView

--- flower_shop/settings.py
+++ flower_shop/settings.py
@@ -126,3 +126,10 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Rest pagination settings
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 100
+}
--- goods/views.py
+++ goods/views.py
@@ -1,14 +1,19 @@
-from django.db import models
+from rest_framework.pagination import LimitOffsetPagination, PageNumberPagination
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from goods.models import Product
 from goods.serializers import ProductListCustomerSerializer 
 
+class PaginatorAPI(PageNumberPagination):
+    page_size = 2
+
 
 class ProductList(APIView):
     def get(self, request):
         products = Product.objects.all()
-        serializer = ProductListCustomerSerializer(products, many=True)
+        paginated_page = PaginatorAPI().paginate_queryset(products, request)
+        serializer = ProductListCustomerSerializer(paginated_page, many=True)
+        
         return Response(serializer.data)
 
