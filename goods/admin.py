6b36d751f798795544a4db9d95b2dc7ba787c287
Nikita Baranov <nbaranov.vrn@gmail.com>
2021-10-22 18:11:15 +0300

    added Favorite model with ManyToMany relationship to Product

--- goods/admin.py
+++ goods/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from goods.models import Product
+from goods.models import Favorite, Product, FavoriteProductList
 
 # Register your models here.
 admin.site.register(Product)
+admin.site.register(Favorite)
+admin.site.register(FavoriteProductList)
--- goods/models.py
+++ goods/models.py
@@ -1,5 +1,7 @@
+from django.contrib.auth.models import User
 from django.db import models
 from django.core.validators import MinValueValidator
+
 
 class Product(models.Model):
     title = models.CharField(max_length=64)
@@ -7,7 +9,23 @@ class Product(models.Model):
     quantity = models.IntegerField(validators=[MinValueValidator(0)])
     price = models.FloatField(validators=[MinValueValidator(0.0)])
 
-
     def __str__(self):
         return f"{self.title}"
 
+
+class Favorite(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    products = models.ManyToManyField(Product, through="FavoriteProductList")
+
+    def __str__(self):
+        queryset = FavoriteProductList.objects.filter(fav_user = self).values('product__title')
+        product_list = [product['product__title'] for product in queryset]
+        return f"{self.user.username}: {', '.join(product_list)}"
+
+
+class FavoriteProductList(models.Model):
+    fav_user = models.ForeignKey(Favorite, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.fav_user.user.username}: {self.product}"
