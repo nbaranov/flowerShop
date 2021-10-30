70698a669108005dec54b9433b35a9fba032ba21
dezalator <alex@korzh.me>
2021-10-30 16:39:58 +0300

    jwt auth

--- flower_shop/settings.py
+++ flower_shop/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework_simplejwt',
     'django_filters',
     'goods',
 ]
@@ -135,5 +136,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 100
+    'PAGE_SIZE': 100,
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
 }
--- goods/utils.py
+++ goods/utils.py
@@ -11,7 +11,6 @@ class Parser(ABC):
 
 class ExcelParser(Parser):
     def parse(self, file):
-        # TODO обработка ошибок, учесть external_id
         work_book = openpyxl.load_workbook(file)
         sheet_names = work_book.sheetnames
         sheet = work_book[sheet_names[0]]
@@ -19,7 +18,7 @@ class ExcelParser(Parser):
         data_table = []
         for row in range(2, sheet.max_row):
             data_row = [sheet.cell(row=row, column=col).value for col in range(1, sheet.max_column+1)]
-            if data_row[0] is None:  # это уже проверяет не пустой ли external_id
+            if data_row[0] is None:
                 continue
             else:
                 data_table.append(data_row)
